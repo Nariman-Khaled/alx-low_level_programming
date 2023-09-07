d14388850222e0eb692210032d199b013c5f4e25
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-08 02:19:02 +0300

    4.put final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main () {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+* main - app main entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
