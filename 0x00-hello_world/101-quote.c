6f861124a6e1dcf248a9ca57aab382f058d4e573
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-08 03:06:37 +0300

    6. updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 size_t message_len = strlen(message);
 write(2, message, message_len);
 return (1);
