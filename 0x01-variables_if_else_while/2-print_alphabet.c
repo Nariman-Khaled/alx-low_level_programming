fac2a6e537d19cff4fe4da96b28949ce89420658
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-09 01:45:54 +0300

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - application entry point
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	char ascii;
+
+	for (ascii = 'a'; ascii <= 'z'; ascii++)
+	{
+		putchar(ascii);
+	}
+	return (0);
+}
