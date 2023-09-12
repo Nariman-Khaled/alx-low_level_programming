563c4511b36a9abf229ac877b3bb49ad57c92db3
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-12 22:10:22 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,21 @@
 */
 int print_sign(int n)
 {
+	int result;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		result = 1;
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
-	} else if (n < 0)
+		result = 0;
+	} else
 	{
 		_putchar('-');
-		return (-1);
+		result = -1;
 	}
+	return (result);
 }
 
