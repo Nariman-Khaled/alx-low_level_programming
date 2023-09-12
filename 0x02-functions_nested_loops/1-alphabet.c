3f22a581e89a179cdb05390be037bba7637446e1
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-12 16:49:58 +0300

    updated main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,6 @@
 #include "_putchar.c"
 
 /**
- * main - application entry point
- *
- * Return: Always 0
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - prints the alphabet
 */
 void print_alphabet(void)
