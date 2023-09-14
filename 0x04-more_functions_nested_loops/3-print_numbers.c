aa84ad70bac66417d6a096f96b4135dbee4b0332
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-14 23:21:57 +0300

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - rints the numbers, from 0 to 9, followed by a new line.
+*/
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		_putchar(number);
+	}
+}
