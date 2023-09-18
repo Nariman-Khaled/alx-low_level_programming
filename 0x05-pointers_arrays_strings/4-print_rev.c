bbc3c65fccfa55619b0fccd5e465867c0511de7b
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-19 02:54:27 +0300

    4-print_rev: long text fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,20 +6,17 @@
 */
 void print_rev(char *s)
 {
-	char buffer[200];
 	int count = 0;
 	int index;
 
-	while (*s != 0)
+	while (*(s + count) != 0)
 	{
-		buffer[count] = *s;
 		count++;
-		s++;
 	}
 
 	for (index = count - 1; index >= 0; index--)
 	{
-		_putchar(buffer[index]);
+		_putchar(s[index]);
 	}
 	_putchar('\n');
 }
