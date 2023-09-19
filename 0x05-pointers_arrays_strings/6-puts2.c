f24dd0f1a6a29a81b5d878dfb954b2a795ea54d7
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-19 03:30:04 +0300

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line.
+ * @str: pointer to string
+*/
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (*str != 0)
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+		count++;
+	}
+	_putchar('\n');
+}
