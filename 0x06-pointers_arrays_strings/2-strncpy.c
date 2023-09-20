2b878e7ff13909a4e18b019be6fa66e8d6fe77f6
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-20 16:04:01 +0300

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncpy -  copies a string.
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated if it contains n or more bytes
+ * @src: the string to copy
+ * @dest: string to be copied to
+ * @n:number of bytes to copy
+ *
+ * Return: pointer to the resulting string dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+	int index = 0;
+
+	printf("_strncpy dest: %s \n", dest);
+
+	for (index = 0; index < n; index++)
+	{
+		if (*src == 0)
+		{
+			*ptr = 0;
+		}
+		else
+		{
+			*ptr = *src;
+			src++;
+		}
+		ptr++;
+	}
+
+	printf("_strncpy end dest: %s \n", dest);
+	return (dest);
+}
