269090bd86b6a0f7ae09172c4cb0f50643781dd4
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-21 19:50:06 +0300

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string -  capitalizes all words of a string.
+ * Separators of words: space, tabulation
+ * , new line, ,, ;, ., !, ?, ", (, ), {, and }
+ * @str: str to change
+ *
+ * Return: pointer to capitalized string.
+*/
+char *cap_string(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != 0)
+	{
+		switch (*ptr)
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+				if (*(ptr + 1) > 96 && *(ptr + 1) < 123)
+				{
+					ptr++;
+					*ptr = *ptr - 32;
+				}
+		}
+		ptr++;
+	}
+
+	return (str);
+}
