d81990b6dfba9bfe56cb7dbd08a9a5d6805bff06
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-09-25 14:10:40 +0300

    _strchr : docs fixx

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string.
- * @s: string to search in
- * @c: search character
- * Return: pointer to the first occurrence of the character c
- * in the string s
- * or NULL if the character is not found.
-*/
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer to a character aka the character found
+ */
 char *_strchr(char *s, char c)
 {
 	unsigned int index = 0;
