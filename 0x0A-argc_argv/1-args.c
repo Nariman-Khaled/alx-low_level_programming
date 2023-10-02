34ee8ad3ae608c90d00fd00c6bc8ecc29213b891
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-03 00:24:50 +0300

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
