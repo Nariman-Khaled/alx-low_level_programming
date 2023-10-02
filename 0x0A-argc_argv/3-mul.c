d76ac271876be49a13c2d2dca9c4f1e715b970db
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-03 00:44:00 +0300

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+
+	return (0);
+}
