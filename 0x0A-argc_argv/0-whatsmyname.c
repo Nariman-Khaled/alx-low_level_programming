1ff8bd0930402ebeed2178f26ad3d688180becb3
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-03 00:13:36 +0300

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ * @argc: the size
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
