b8641254808d2db4bbd8abb2b8f0f3db155b59e0
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-03 00:37:21 +0300

    2-args fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; *argv; i++, argv++)
-	{
-		printf("%s\n", *argv);
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
 	return (0);
 }
