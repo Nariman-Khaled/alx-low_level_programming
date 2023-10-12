1b7ba21b534e976781f966125c63dad04b71c4f0
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-12 12:51:03 +0300

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -  returns the sum of all its parameters
+ * @n: number of elemets to cout
+ *
+ * Return: count of the arguments
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int count = 0;
+
+	if (n <= 0)
+		return (0);
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int value = va_arg(args, int);
+
+		count += value;
+	}
+
+	va_end(args);
+
+	return (count);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _Variadic_H_
+#define _Variadic_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
